8e2be0fd2422b359c183829efa9068a7e5e0ede1
Ian Nguyen <iannguyen172@gmail.com>
2020-02-20 17:04:34 +1100

    Added and styled headers/footers

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,9 @@
 import React from 'react';
+
+const date = new Date().getFullYear();
+
+function Footer(){
+    return (<footer><p>Copyright &copy; {date} Ian Nguyen </p></footer>);
+}
+
+export default Footer;
